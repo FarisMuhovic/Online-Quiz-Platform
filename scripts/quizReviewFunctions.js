cfdd933aab7fde6335c43d4a9aaf3c24f4864b38
Faris Muhovic <faris.fare123456@gmail.com>
2024-03-30 17:36:56 +0100

    small fix for colors

--- scripts/quizReviewFunctions.js
+++ scripts/quizReviewFunctions.js
@@ -8,9 +8,12 @@ export const fetchQuizReview = () => {
       <h3>${data.category}</h3>
       <p>Your score: ${data.correctAnswers}/${data.questionCount}</p>
       <h3>Color info:</h3>
-      <p>Red: your answer (wrong)</p>
-      <p>Green: right answer, and your answer.</p>
-    </section>
+      <div class="info-div">
+        <p>Red: your answer (wrong)</p>
+        <p>Green: right answer, and your answer.</p>
+        <p>Note: if every answer is red(except right one), that means you didnt answer at all.</p>
+      </div>
+      </section>
     `
     data.answers.forEach(answer => {
       quizReviewContainer.innerHTML += `
@@ -26,10 +29,19 @@ export const fetchQuizReview = () => {
 }
 const insertAnswersHTML = answer => {
   let answersHTML = ""
-  answer.fields.forEach(field => {
-    answersHTML += `<p class="answer ${field.correct ? "correct" : ""} ${
-      answer.userAnswer == field.text ? "user-answer" : ""
-    }">${field.text}</p>`
-  })
+
+  if (answer.userAnswer === "") {
+    answer.fields.forEach(field => {
+      answersHTML += `<p class="answer ${
+        field.correct ? "correct" : ""
+      } user-answer">${field.text}</p>`
+    })
+  } else {
+    answer.fields.forEach(field => {
+      answersHTML += `<p class="answer ${field.correct ? "correct" : ""} ${
+        answer.userAnswer == field.text ? "user-answer" : ""
+      }">${field.text}</p>`
+    })
+  }
   return answersHTML
 }
