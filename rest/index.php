d05fc9ef148dd029e8aea78f3b7d61c4df244a11
Faris Muhovic <faris.fare123456@gmail.com>
2024-05-20 15:55:01 +0200

    hosting

--- rest/index.php
+++ rest/index.php
@@ -6,11 +6,22 @@ require 'routes/historyRoutes.php';
 require 'routes/userRoutes.php';
 require 'routes/quizRoutes.php';
 
-Flight::route('OPTIONS /*', function(){
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: PUT, DELETE");
+// Define your allowed origin
+$allowedOrigin = "https://quizapp.farismuhovic.online";
+
+// Handle OPTIONS requests
+Flight::route('OPTIONS /*', function() use ($allowedOrigin) {
+    header("Access-Control-Allow-Origin: $allowedOrigin");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     exit;
+});
+
+// Handle all other requests
+Flight::before('start', function() use ($allowedOrigin) {
+    header("Access-Control-Allow-Origin: $allowedOrigin");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
 });
 
 Flight::start();
